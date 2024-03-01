1cdd8a1fbd789747b58d4c304edd4b63b08dc5d0
Diego Rebollo <130008792+diego-52h@users.noreply.github.com>
2024-03-01 15:12:38 -0300

    Delete engine directory

--- engine/collision/CollisionVolume.cxx
+++
@@ -1,7 +0,0 @@
-#include "CollisionVolume.hxx"
-
-GameNode& CollisionVolume::getParent() { return this -> parent; }
-
-const Transform& CollisionVolume::getTransform() const { return this -> parent.getTransform(); }
-
-CollisionVolume::CollisionVolume(const Transform& transform, const Vector2& maxSize) : transform(transform), maxSize(maxSize) { }
--- engine/graphics/Camera.cxx
+++
@@ -1,5 +0,0 @@
-#include "Camera.hxx"
-
-Camera::Camera(Node& parent) : parent(parent) { }
-
-Transform& Camera::getTransform() { return this -> transform; }
